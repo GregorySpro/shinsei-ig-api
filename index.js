f5e491a8036d5dc2de1c2454a5463a255be4a68a
Grégory Sergent <gregorysergentpro@gmail.com>
2025-08-13 15:55:42 +0200

    added base.

---
+++ index.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const express = require('express');
+const { Pool } = require('pg');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+app.use(express.json());
+
+app.get('/users', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM users'); // adapte selon ta table
+    res.json(result.rows);
+  } catch (error) {
+    console.error(error);
+    console.log('DATABASE_URL =', process.env.DATABASE_URL);
+    res.status(500).send('Erreur serveur');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`API en écoute sur le port ${port}`);
+});
