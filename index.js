48c88c3032938cac9e56c3a8b96938da0fc9f697
Grégory Sergent <gregorysergentpro@gmail.com>
2025-08-14 13:26:04 +0200

    added cors.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const { Pool } = require('pg');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3000;
+app.use(cors());
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
