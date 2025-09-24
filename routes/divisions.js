f270728883d6366f4b2c62f489bdd5f25b9e2809
Grégory Sergent <gregorysergentpro@gmail.com>
2025-09-24 16:08:47 +0200

    debugging.

--- routes/divisions.js
+++ routes/divisions.js
@@ -33,6 +33,7 @@ router.get('/divisions/get_user_division', authMiddleware, async (req, res) => {
       return res.status(404).json({ message: 'Utilisateur non trouvé.' });
     } 
     const userDivisionId = userResult.rows[0].division;
+    console.log('userDivisionId:', userDivisionId); // Pour déboguer
     if (!userDivisionId) {
       return res.status(200).json({ message: 'L\'utilisateur n\'appartient à aucune division.' });
     } 
